698eb2811721b86c9d1f32aad85fa8e6f5181f88
Wanga-tracker <trackerwanga01@gmail.com>
2025-06-01 04:43:44 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjsxzqXwW2PcwowbFSb4jiGTHGv9rdL0w",
+  authDomain: "agent-hive-project.firebaseapp.com",
+  databaseURL: "https://agent-hive-project-default-rtdb.firebaseio.com",
+  projectId: "agent-hive-project",
+  storageBucket: "agent-hive-project.firebasestorage.app",
+  messagingSenderId: "464476218600",
+  appId: "1:464476218600:web:59aa5ba90c73468d78155d",
+  measurementId: "G-YGQ85D592J"
+};
+
+firebase.initializeApp(firebaseConfig);
