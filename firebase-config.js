3315612c3ee461510f358be86180e600dc9f326b
Wanga-tracker <trackerwanga01@gmail.com>
2025-06-01 08:21:16 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,18 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-storage.js";
+// firebase-config.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.8.2/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/11.8.2/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.8.2/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/11.8.2/firebase-storage.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAjsxzqXwW2PcwowbFSb4jiGTHGv9rdL0w",
   authDomain: "agent-hive-project.firebaseapp.com",
-  databaseURL: "https://agent-hive-project-default-rtdb.firebaseio.com",
   projectId: "agent-hive-project",
   storageBucket: "agent-hive-project.firebasestorage.app",
   messagingSenderId: "464476218600",
   appId: "1:464476218600:web:59aa5ba90c73468d78155d",
   measurementId: "G-YGQ85D592J"
 };
-
-firebase.initializeApp(firebaseConfig);
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
